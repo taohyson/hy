1edc420a0a6c5645fd1f83fdedf509d52897a609
taohyson <tao.hyson@gmail.com>
2013-10-11 10:09:33 +0800

    1.服务器停止类提交

---
+++ ServerStop.java
@@ -0,0 +1,15 @@
+package ttt.scene;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * 服务器关闭, 保存数据.
+ * @author luch
+ */
+@Component
+public class ServerStop {
+
+    public void destroy() {
+
+    }
+}
